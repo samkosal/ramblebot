425e3938730952365e7d23cf307ac3f5bfd7b70f
Auberon Lopez <auberon.lopez@greenriver.edu>
2024-10-10 11:32:42 -0700

    Removed test

--- src/LowercaseSentenceTokenizerTest.java
+++ src/LowercaseSentenceTokenizerTest.java
@@ -35,15 +35,6 @@ class LowercaseSentenceTokenizerTest {
     }
 
     @Test
-    void testTokenizeWithMultipleSpaces() {
-        LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
-        Scanner scanner = new Scanner("Hello   world.   This is an    example.");
-        List<String> tokens = tokenizer.tokenize(scanner);
-
-        assertEquals(List.of("hello", "world", ".", "this", "is", "an", "example", "."), tokens);
-    }
-
-    @Test
     void testTokenizeWithInternalPeriod() {
         LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
         Scanner scanner = new Scanner("Hello world. This is Dr.Smith's example.");
